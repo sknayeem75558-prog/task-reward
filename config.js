4e81327454add06d47cb598c616b439917e5e0b1
sknayeem75558-prog <sknayeem75558@gmail.com>
2026-09-02 16:20:41 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const SUPABASE_URL = "https://crdxxiwtfxpbomcvdqjk.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_u9W9dmz4t5f6vx9cY1kBMw_ZBiGZJA2";
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
